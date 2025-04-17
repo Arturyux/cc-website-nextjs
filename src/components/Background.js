766e89d173110cfa34e48a44930536d51eb99f5e
Arturyux <artur.burlakin@gmail.com>
2025-04-17 13:43:35 +0200

    adding new patern to the main bg

--- src/components/Background.js
+++ src/components/Background.js
@@ -3,11 +3,12 @@
 const patterns = [
   "/patterns/patern.svg",
   "/patterns/pattern2.svg",
+  "/patterns/pattern3.svg"
 ];
 
-// Select a specific pattern from the array (for example, index 0)
 const selectedPattern = patterns[0];
 const selectedPattern1 = patterns[1];
+const selectedPattern2 = patterns[2];
 
 export default function Background() {
   return (
@@ -16,8 +17,14 @@ export default function Background() {
       <div className="absolute md:w-[8%] w-[15%] md:top-45 md:left-30 top-35 left-0 md:rotate-0 rotate-45 -z-1">
         <img src={selectedPattern} alt="Background Pattern" />
       </div>
-      <div className="absolute md:top-170 md:left-150 md:w-[20%] w-[70%] top-170 left-70 -z-1">
+      <div className="absolute md:top-170 md:left-150 md:w-[20%] w-[70%] top-150 left-70 -z-1">
         <img src={selectedPattern1} alt="Background Pattern" />
+      </div>
+      <div className="absolute md:top-380 md:left-280 md:w-[10%] w-[30%] top-350 left-90 -z-1 rotate-220">
+        <img src={selectedPattern} alt="Background Pattern" />
+      </div>
+      <div className="absolute md:top-270 md:left-45 md:w-[20%] w-[70%] top-205 -left-15 -z-1 md:rotate-240 rotate-260">
+        <img src={selectedPattern2} alt="Background Pattern" />
       </div>
     </div>
     </>
